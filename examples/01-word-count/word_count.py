5a7f38183eb6d5d96d3b287f8eca3a54baa7193f
Gaurav Agarwal <algogrit@gmail.com>
2024-08-07 12:31:23 +0530

    Adding a word count example. TODO: Change the output format.

--- examples/01-word-count/word_count.py
+++ examples/01-word-count/word_count.py
@@ -1,9 +1,27 @@
-sc = SparkContext.getOrCreate()
+from pyspark.sql import SparkSession
 
-input_file = "/Users/gaurav/Developer/Training/Spark/README.md"
-text_rdd = sc.textFile(input_file)
+import time
 
-word_counts = text_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+def main():
+    spark = SparkSession.builder \
+        .appName("Word Count") \
+        .getOrCreate()
 
-output_dir = "output"
-word_counts.saveAsTextFile(output_dir)
+    sc = spark.sparkContext
+
+    input_file = "/app/examples/good_data_sets.md"
+    text_rdd = sc.textFile(input_file)
+
+    word_counts = text_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+    output_dir = "output"
+    word_counts.saveAsTextFile(output_dir)
+
+    print(word_counts)
+
+    time.sleep(10000)
+
+    sc.stop()
+
+if __name__ == "__main__":
+    main()
