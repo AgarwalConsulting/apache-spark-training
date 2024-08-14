ebf386132eedc7c3558f146d47cd7cc7b3ea60b7
Gaurav Agarwal <algogrit@gmail.com>
2024-08-14 12:49:01 +0530

    Adding the output to Avro.

---
+++ examples/airline_delays/flight_df_avro.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+import time
+
+spark = SparkSession.builder \
+    .appName("simple-python-starters") \
+    .getOrCreate()
+
+
+df = spark.read.csv("data_sets/airline/Airline_Delay_Cause.csv", header=True, inferSchema=True)
+
+carrier_grouped_df = df.groupBy('carrier_name').agg(
+    F.sum('arr_flights').alias('total_flights'),
+    F.sum('arr_del15').alias('total_delays')
+)
+
+carrier_grouped_df = carrier_grouped_df.withColumn(
+    'delay_percentage',
+    (F.col('total_delays') / F.col('total_flights')) * 100
+)
+
+sorted_carrier_df = carrier_grouped_df.orderBy(F.col('delay_percentage').desc())
+
+
+sorted_carrier_df.write.format("avro").save("output/carrier_delays_percentage.avro")
+
+# time.sleep(100000)
+
+spark.stop()
--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -5,3 +5,5 @@ rm -rf output
 docker run --name spark-job -it --rm -p 14040:4040 -v "$(pwd):/app/" -w /app apache/spark /opt/spark/bin/spark-submit /app/examples/01-word-count/word_count.py
 
 # docker run --name spark-job-1 -it --rm -p 14041:4040 -v "$(pwd):/app/" -w /app apache/spark /opt/spark/bin/spark-submit /app/examples/simple-python-starters/sum_of_multiples/01-rdd.py
+
+docker run --name spark-job -it --rm -p 14040:4040 -v "$(pwd):/app/" -w /app apache/spark /opt/spark/bin/spark-submit --packages org.apache.spark:spark-avro_2.12:3.5.2 --conf spark.driver.extraJavaOptions="-Divy.cache.dir=/tmp -Divy.home=/tmp" /app/examples/airline_delays/flight_df_avro.py
